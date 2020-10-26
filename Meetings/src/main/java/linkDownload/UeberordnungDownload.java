f5291aafd92d7c258d677b023c1f076761ce7fa0
Alfred Wutschka <hixels@gmx.de>
2020-10-26 11:47:29 +0100

    added Comments

--- Meetings/src/main/java/linkDownload/UeberordnungDownload.java
+++ Meetings/src/main/java/linkDownload/UeberordnungDownload.java
@@ -44,18 +44,23 @@ public class UeberordnungDownload {
 		SqlManager sqlManager = new SqlManager("jdbc:mariadb://localhost/meetings", "root", password);
 		ResultSet resultSet = null;
 
+		//looking for found links which has to be downloaded
 		resultSet = sqlManager.executeSql("SELECT * FROM ueberordnungen WHERE status=10");
 
+		//limiting work done for testing purpose
 		int Anzahl = 2;
 		while (resultSet.next()) {
 			System.out.println("Verarbeite: '" + resultSet.getString("ID") + "', '" + resultSet.getString("URL") + "'");
 			Kongress it = new Kongress(resultSet.getString("URL"));
 			// der eigentliche Aufruf
 			String kongressDir = mainPath + "kongresse" + fs + it.kurzID + it.languageSpec + fs;
+			
+			//downloading the Website and all prerequisites on harddisk
 			MyWget myWget = new MyWget(it.url, kongressDir, true);
 			@SuppressWarnings("unused")
 			int res = myWget.getPage();
 
+			// Extracting which websites give aditional Infos...
 			File kongressFile = new File(myWget.getTarget());
 			Document doc = Jsoup.parse(kongressFile, "CP1252", protokoll + hostname);//
 			Elements elements = doc.getElementById("owner_links").children();
@@ -65,6 +70,7 @@ public class UeberordnungDownload {
 					owner_links.add(listenEintrag.getElementsByTag("a").first().attr("href"));
 				}
 			}
+			//... and download them too
 			MyWget[] contentMyWget = new MyWget[owner_links.size()];
 			String[] contentPath = new String[owner_links.size()];
 			for (int i = 0; i < owner_links.size(); i++) {
@@ -74,6 +80,7 @@ public class UeberordnungDownload {
 				contentMyWget[i].getPage();
 			}
 
+			//mix all those prerequisites, to be able to merge the websites
 			File dest = new File(kongressDir + fs + "merge" + fs + "content" + fs + hostname + fs);
 			dest.mkdirs();
 			if (dest.exists()) {
@@ -84,6 +91,7 @@ public class UeberordnungDownload {
 				MyUtils.copyFolder(new File(contentPath[i] + "content" + fs + hostname + fs), dest);
 			}
 
+			//merge the checksum Files (actually isn't needed yet, just to keep the standard)
 			SortedSet<String> lines = new TreeSet<>();
 			BufferedReader br = new BufferedReader(
 					new FileReader(new File(kongressDir + "content" + fs + "checksum.md5")));
@@ -108,12 +116,11 @@ public class UeberordnungDownload {
 			}
 			bw.close();
 
+			//take the "landingpage" of the congress
 			File htmlFile = new File(kongressDir + "content" + fs + "target.html");
-			doc = Jsoup.parse(htmlFile, "CP1252", protokoll + hostname);// vorher: "ISO-8859-1"
-//			doc.outputSettings().syntax(org.jsoup.nodes.Document.OutputSettings.Syntax.xml);//wofür wa das?
-//			doc.outputSettings().charset("CP1252");//vorher: "UTF-8"
-//			doc.outputSettings().charset("ISO-8859-1");
+			doc = Jsoup.parse(htmlFile, "CP1252", protokoll + hostname);
 
+			//and get rid of all stuff which is for the website, but what we don't want for the PDF
 			Element content = doc.child(0);
 			content.getElementById("navigation_language").remove();
 			content.getElementById("navigation").remove();
@@ -122,10 +129,10 @@ public class UeberordnungDownload {
 			content.getElementsByTag("script").remove();
 			Document[] newDoc = new Document[10];
 			Element[] newElement = new Element[10];
+			//adding the content from the websites with further Information to this merge-html File created
 			for (int i = 0; i < owner_links.size(); i++) {
 				content.getElementById("content").append("<div class=\"clear\">&nbsp;</div>");
-				newDoc[i] = Jsoup.parse(new File(contentPath[i] + "content" + fs + "target.html"), "CP1252", // vorher:
-																												// "ISO-8859-1"
+				newDoc[i] = Jsoup.parse(new File(contentPath[i] + "content" + fs + "target.html"), "CP1252",
 						protokoll + hostname);
 				newElement[i] = newDoc[i].getElementById("owner");
 				while (newElement[i].nextElementSibling() != null) {
@@ -133,19 +140,15 @@ public class UeberordnungDownload {
 					content.getElementById("content").append(newElement[i].html());
 				}
 			}
+			//and save it to harddisk
 			FileOutputStream fstream = new FileOutputStream(
 					kongressDir + "merge" + fs + "content" + fs + "target.html");
 			OutputStreamWriter out = new OutputStreamWriter(fstream, "windows-1252");
 			out.append(doc.html());
 			out.close();
 			fstream.close();
-			/*
-			 * bw = new BufferedWriter( // new FileWriter(kongressDir +
-			 * "merge/content/target.html", // Charset.forName("ISO-8859-1"))); new
-			 * FileWriter(kongressDir + "merge" + fs + "content" + fs + "target.html"));
-			 * bw.append(doc.html()); bw.close();
-			 */
 
+			//check if this one css File is the known one and if yes: replace it with a different one, made for PDF purpose
 			File cssFile = new File(kongressDir + "merge" + fs + "content" + fs + "www.egms.de" + fs + "static" + fs
 					+ "css" + fs + "gms-framework.css");
 			if (!MyUtils.md5_of_file(cssFile).equals("532d9c009619553ea5841742ac59b2df")) {
@@ -154,59 +157,58 @@ public class UeberordnungDownload {
 			File cssFileSrc = new File(mainPath + "gms-framework.css");
 			Files.copy(cssFileSrc.toPath(), cssFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
+			//The "LandingPage" of the congress always have a sessionlist, with websites, where the links to the abstracts can be found
 			Elements sessionlist = content.getElementsByClass("sessionlist").first().getElementsByTag("a");
 			int i = 0;
+			//again: limit for testing purpose
 			int Anzahl2 = 1;
+			//Go through them, to extract the links of the abstracts (not actually for "landingPage" of the congress download purpose, but because it is needed later)
 			for (Element session : sessionlist) {
 				String kongressDir2 = mainPath + "kongresse" + fs + it.kurzID + it.languageSpec + fs + "abstractlist"
 						+ ++i;
+				//download the Sessions from the Sessionlist
 				MyWget myWget2 = new MyWget(session.attr("href"), kongressDir2, true);
 				@SuppressWarnings("unused")
 				int res2 = myWget2.getPage();
 
 				File abstractlistFile = new File(myWget2.getTarget());
-				Elements abstractlist = Jsoup.parse(abstractlistFile, "CP1252", protokoll + hostname).child(0)// vorher:
-																												// "ISO-8859-1"
+				Elements abstractlist = Jsoup.parse(abstractlistFile, "CP1252", protokoll + hostname).child(0)
 						.getElementsByClass("hx_link");
 
-				// System.out.println("hier:");
-				// System.out.println(Jsoup.parse(abstractlistFile, "ISO-8859-1", protokoll +
-				// hostname).child(0));
+				//go through the abstracts in a session
 				for (Element abstractElement : abstractlist) {
 					Abstract aAbstract = new Abstract(abstractElement.attr("href"));
 					ResultSet resultSet2 = sqlManager.executeSql("SELECT * FROM abstracts WHERE Ab_ID = '"
 							+ aAbstract.Ab_ID + "_" + aAbstract.language + "'");
-					// Prüfe, ob sich bereits ein solcher Eintrag in der Datenbank befindet
+					//Check if the abstract was in the Database before
 					if (resultSet2.next()) {
-						// War schon drin
+						// skip if yes
 					} else {
-						// Füge ein
-						// System.out.println("Verarbeite: '" + it.kurzID + "', '" + it.url + "'");
+						// otherwise insert it
 						resultSet2 = sqlManager
 								.executeSql("INSERT INTO abstracts (Ue_ID, Ab_ID , URL, Status) VALUES (\""
 										+ aAbstract.Ue_ID + "_" + aAbstract.language + "\", \"" + aAbstract.Ab_ID + "_"
 										+ aAbstract.language + "\", \"" + aAbstract.url + "\", 10);");
 						if (0 == --Anzahl2)
-							break; // tu nicht zu viel
+							break; // don't do too much (for testing purpose)
 					}
 				}
 				if (0 == Anzahl2)
-					break; // tu nicht zu viel
+					break; // don't do too much (for testing purpose)
 			}
 
-			// Den Fortschritt in der Datenbank vermerken
+			// assign this congress as downloaded
 			int updated = sqlManager
 					.executeUpdate("UPDATE ueberordnungen SET Status = 30 WHERE ID = '" + it.kurzID + "_" + it.language + "';");
-			// int updated = 1;// zum testen
 			if (updated != 1)
 				System.err.println("Es sollte sich nun genau eine Zeile aktualisiert haben unter der KurzID '"
 						+ it.kurzID + "_" + it.language + "', aber es waren: " + updated + ".");
-			// Nicht zu schnell eine Anfrage nach der Anderen
+			// For less Traffic, wait after every congress
 			System.out.flush();
 			TimeUnit.SECONDS.sleep(1);
 
 			if (0 == --Anzahl)
-				break; // Tue nicht zu viel
+				break; // don't do too much (for testing purpose)
 		}
 
 	}
