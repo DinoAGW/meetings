a3381917a51d2a5a8f00cbb5ff87029fa64a47aa
Carl Wilson <carl@openpreservation.org>
2020-12-01 15:08:23 +0000

    Revert "Revert "REFACT: Don't Repeat Yourself DRY""

--- Meetings/src/main/java/utilities/Clean.java
+++ Meetings/src/main/java/utilities/Clean.java
@@ -2,28 +2,34 @@ package utilities;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Clean {
-	static String fs = System.getProperty("file.separator");
+	static final String fs = System.getProperty("file.separator");
+	public static final String mainPath = System.getProperty("user.home").concat(fs).concat(".meetings").concat(fs);
 
-	public static void main(String[] args) throws IOException, SQLException {
+	public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException {
 		//Ordner leeren
-		String mainPath = "C:\\Users\\hixel\\workspace\\Meetings\\Ueberordnungen\\";
-		Utilities.deleteDir(mainPath + "kongresse");
-		Utilities.deleteDir(mainPath + "landingPage");
+		String captPath = mainPath.concat("Ueberordnungen").concat(fs);
+		Utilities.deleteDir(captPath.concat("kongresse"));
+		Utilities.deleteDir(captPath.concat("landingPage"));
 		
-		mainPath = "C:\\Users\\hixel\\workspace\\Meetings\\Abstracts\\";
-		Utilities.deleteDir(mainPath + "kongresse");
+		String absPath = mainPath.concat("Abstracts").concat(fs);
+		Utilities.deleteDir(absPath.concat("kongresse"));
 		//Utilities.deleteDir(mainPath + "landingPage");
 
 		//urls Tabelle leeren
-		String propertypfad = System.getProperty("user.home") + fs + "properties.txt";
-		String password = Utilities.readStringFromProperty(propertypfad, "password");
-		SqlManager sqlManager = new SqlManager("jdbc:mariadb://localhost/meetings", "root", password);
+		// String propertypfad = System.getProperty("user.home") + fs + "properties.txt";
+		// String password = Utilities.readStringFromProperty(propertypfad, "password");
+		try {
+			Statement stmt = SqlManager.getConnection().createStatement();
 
-		sqlManager.executeUpdate("DELETE FROM ueberordnungen;");
-		sqlManager.executeUpdate("DELETE FROM abstracts;");
-		
+			stmt.executeUpdate("CREATE TABLE ueberordnungen (ID VARCHAR(20), URL VARCHAR (200), Status INT );");
+			stmt.executeUpdate("CREATE TABLE abstracts (Ue_ID VARCHAR(20), Ab_ID VARCHAR(20), URL VARCHAR (200), Status INT );");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("Clean Ende.");
 	}
 
